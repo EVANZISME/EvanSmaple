8bcfbf6d464e529c0d22f06a36598ff3c228f699
sinyenevan <evan.chang@sinyentech.com>
2021-02-03 17:18:38 +0800

    加入專案檔案。

---
+++ ImageHash/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ImageHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string image1 = @"C:\Users\admin\Downloads\EtCpGN9U0AEl8Zf.jpg";
+            string image2 = @"C:\Users\admin\Downloads\EtCpGN9U0AEl8Zf (1).jpg";
+
+
+            var hash1 = new SimilarPhoto(image1).GetHash();
+            var hash2 = new SimilarPhoto(image2).GetHash();
+
+            var comp = SimilarPhoto.CalcSimilarDegree(hash1, hash2);
+
+
+            Console.WriteLine($"差異度:{comp}");
+        }
+    }
+}
